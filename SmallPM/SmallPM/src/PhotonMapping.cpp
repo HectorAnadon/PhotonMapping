a3a7550e167377fb6e53cb603f7362bd1dc92e95
Alberto Sabater <asabater94@gmail.com>
2016-01-08 17:15:02 +0100

    First commit

--- SmallPM/SmallPM/src/PhotonMapping.cpp
+++ SmallPM/SmallPM/src/PhotonMapping.cpp
@@ -4,9 +4,9 @@ Copyright (C) 2014 Diego Gutierrez (diegog@unizar.es)
 All rights reserved.
 
 This is an educational Ray Tracer developed for the course 'Informatica Grafica'
-(Computer Graphics) tought at Universidad de Zaragoza (Spain). As such, it does not 
+(Computer Graphics) tought at Universidad de Zaragoza (Spain). As such, it does not
 intend to be fast or general, but just to provide an educational tool for undergraduate
-students. 
+students.
 
 This software is provided as is, and any express or implied warranties are disclaimed.
 In no event shall copyright holders be liable for any damage.
@@ -16,6 +16,7 @@ In no event shall copyright holders be liable for any damage.
 #include "Intersection.h"
 #include "Ray.h"
 #include "BSDF.h"
+#include <iostream>
 
 //*********************************************************************
 // Compute the photons by tracing the Ray 'r' from the light source
@@ -31,36 +32,36 @@ In no event shall copyright holders be liable for any damage.
 // The function will return true when there are more photons (caustic
 // or diffuse) to be shot, and false otherwise.
 //---------------------------------------------------------------------
-bool PhotonMapping::trace_ray(const Ray& r, const Vector3 &p, 
-			   std::list<Photon> &global_photons, std::list<Photon> &caustic_photons, bool direct)
+bool PhotonMapping::trace_ray(const Ray& r, const Vector3 &p,
+	std::list<Photon> &global_photons, std::list<Photon> &caustic_photons, bool direct)
 {
 
 	//Check if max number of shots done...
-	if( ++m_nb_current_shots > m_max_nb_shots )
+	if (++m_nb_current_shots > m_max_nb_shots)
 	{
 		return false;
 	}
-	
+
 	// Compute irradiance photon's energy
 	Vector3 energy(p);
-	
+
 	Ray photon_ray(r);
 	photon_ray.shift();
 
 	bool is_caustic_particle = false;
 
 	//Iterate the path
-	while(1)
+	while (1)
 	{
 		// Throw ray and update current_it
 		Intersection it;
 		world->first_intersection(photon_ray, it);
 
-		if( !it.did_hit() )
+		if (!it.did_hit())
 			break;
 
 		//Check if has hit a delta material...
-		if( it.intersected()->material()->is_delta() )
+		if (it.intersected()->material()->is_delta())
 		{
 			// If delta material, then is caustic...
 			// Don't store the photon!
@@ -69,53 +70,59 @@ bool PhotonMapping::trace_ray(const Ray& r, const Vector3 &p,
 		else if (photon_ray.get_level() > 0 || direct)
 		{
 			//If non-delta material, store the photon!
-			if( is_caustic_particle )	
-			{				
+			if (is_caustic_particle)
+			{
 				//If caustic particle, store in caustics
-				if( caustic_photons.size() < m_nb_caustic_photons )
-					caustic_photons.push_back( Photon(it.get_position(), photon_ray.get_direction(), energy ));
+				if (caustic_photons.size() < m_nb_caustic_photons)
+					caustic_photons.push_back(Photon(it.get_position(), photon_ray.get_direction(), energy));
 			}
-			else						
+			else
 			{
 				//If non-caustic particle, store in global
-				if( global_photons.size() < m_nb_global_photons )
-					global_photons.push_back( Photon(it.get_position(), photon_ray.get_direction(), energy ));
+				if (global_photons.size() < m_nb_global_photons)
+					global_photons.push_back(Photon(it.get_position(), photon_ray.get_direction(), energy));
 			}
 			is_caustic_particle = false;
-		}	
-		
+		}
+
 		Real pdf;
 
 		Vector3 surf_albedo = it.intersected()->material()->get_albedo(it);
 		Real avg_surf_albedo = surf_albedo.avg();
 
-		Real epsilon2 = static_cast<Real>(rand())/static_cast<Real>(RAND_MAX);
+		Real epsilon2 = static_cast<Real>(rand()) / static_cast<Real>(RAND_MAX);
 		while (epsilon2 < 0.)
-			epsilon2 = static_cast<Real>(rand())/static_cast<Real>(RAND_MAX);
-		
-		if (epsilon2 > avg_surf_albedo || photon_ray.get_level() > 20 ) 
+			epsilon2 = static_cast<Real>(rand()) / static_cast<Real>(RAND_MAX);
+
+		if (epsilon2 > avg_surf_albedo || photon_ray.get_level() > 20)
 			break;
-			
+
 		// Random walk's next step
 		// Get sampled direction plus pdf, and update attenuation
-		it.intersected()->material()->get_outgoing_sample_ray(it, photon_ray, pdf );
+		it.intersected()->material()->get_outgoing_sample_ray(it, photon_ray, pdf);
 
 		// Shade...
 		energy = energy*surf_albedo;
-		if( !it.intersected()->material()->is_delta() )
-			energy *= dot_abs(it.get_normal(), photon_ray.get_direction())/3.14159;		
+		if (!it.intersected()->material()->is_delta())
+			energy *= dot_abs(it.get_normal(), photon_ray.get_direction()) / 3.14159;
 
-		energy = energy /(pdf*avg_surf_albedo);
+		energy = energy / (pdf*avg_surf_albedo);
 	}
-	
-	if( caustic_photons.size() == m_nb_caustic_photons && 
-		global_photons.size() == m_nb_global_photons )
+
+	if (caustic_photons.size() == m_nb_caustic_photons &&
+		global_photons.size() == m_nb_global_photons)
 	{
-		m_max_nb_shots = m_nb_current_shots-1;
+		m_max_nb_shots = m_nb_current_shots - 1;
 		return false;
 	}
 
 	return true;
+}
+
+double fRand(double fMin, double fMax)
+{
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f* (fMax - fMin);
 }
 
 //*********************************************************************
@@ -134,6 +141,65 @@ bool PhotonMapping::trace_ray(const Ray& r, const Vector3 &p,
 //---------------------------------------------------------------------
 void PhotonMapping::preprocess()
 {
+
+	std::list<Photon> globalPhotons;
+	std::list<Photon> causticPhotons;
+	Ray* photonRay;
+	Vector3 photonFlux(1);	// energia foton = 1
+
+	// Muestrea las fuentes de luz de la escena
+	for (int i = 0; i < world->nb_lights(); i++){
+
+		// Obtiene la fuente de luz i-esima
+		Vector3 lightPos = world->light(i).get_position();
+		Vector3 lightIntensity = world->light(i).get_intensities();
+		LightSource* lt = new PointLightSource(world, lightPos, lightIntensity);
+
+		// Muestreo de una esfera
+		//while (m_nb_current_shots < m_max_nb_shots)
+		do
+		{
+
+			// Genera dos angulos aleatoriamente para obtener la direccion del rayo
+			double omega(fRand(0.0, 2 * 3.14));
+			double theta(fRand(0.0, 2 * 3.14));
+
+			// Calcula la direccion en base a dos angulos (omega y theta)
+			double x = cos(theta) * sin(omega);
+			double y = cos(theta) * cos(omega);
+			double z = sin(theta);
+			Vector3 photonDir(x, y, z);
+
+			// Crea el rayo (foton) a lanzar
+			photonRay = new Ray(lightPos, photonDir);
+
+			// Lanza los fotones muestreados
+
+			//trace_ray(*photonRay, photonFlux, globalPhotons, causticPhotons, false);
+
+
+			// Actualiza el numero de fotones muestreados - trace_ray parece ya aumentarlo cada vez D:
+			//m_nb_current_shots++;
+		} while (trace_ray(*photonRay, photonFlux, globalPhotons, causticPhotons, false));
+
+	}
+
+	while (globalPhotons.size() > 0) {
+		Photon photon = globalPhotons.front();
+		globalPhotons.pop_front();
+		m_global_map.store(std::vector<Real>(photon.position.data,
+			photon.position.data + 3), photon);
+		m_global_map.balance();
+	}
+
+	while (causticPhotons.size() > 0) {
+		Photon photon = causticPhotons.front();
+		causticPhotons.pop_front();
+		m_caustics_map.store(std::vector<Real>(photon.position.data,
+			photon.position.data + 3), photon);
+		m_caustics_map.balance();
+	}
+
 }
 
 //*********************************************************************
@@ -149,8 +215,78 @@ void PhotonMapping::preprocess()
 //---------------------------------------------------------------------
 Vector3 PhotonMapping::shade(Intersection &it0)const
 {
-	Vector3 L(0);
+
+	// ESTRUCTURA
+	// -----------------------------------------------------------
+	// 1.- Calcular iluminacion directa en el punto (a cada PL)
+	// 2.- Calcular iluminacion indirecta en el punto a traves
+	//		de la estimacion de radiancia
+	// -----------------------------------------------------------
+	// Debugueo: poner por separado ID e II
+
+	Vector3 L(0);	// color inicial (fondo negro) ->>>>> mirar funcion get_background() de world.h
 	Intersection it(it0);
+	Vector3 pI = it.get_position();	// punto de interseccion (x,y,z)
+	Vector3 pN = it.get_normal(); // normal en el punto de interseccion
+
+	// TERMINO AMBIENTAL
+	//L = world->get_ambient() * it.intersected()->material()->get_albedo(it);
+
+	// LUZ DIRECTA //
+	for (int i = 0; i < world->nb_lights(); i++){
+
+		// Obtiene la fuente de luz i-esima
+		Vector3 lightPos = world->light(i).get_position();
+		Vector3 lightIntensity = world->light(i).get_intensities();
+		LightSource* lt = new PointLightSource(world, lightPos, lightIntensity);
+
+		Vector3 shadowRay = Vector3() - lt->get_incoming_direction(pI); // (0,0,0) - lightRay = shadowRay
+
+		// Si el objeto es visible se calcula la influencia de la luz
+		if (lt->is_visible(pI)) {
+
+			// TERMINO DIFUSO = Kd x Id x (L . N)
+			Vector3 Id = lt->get_incoming_light(pI);
+			Vector3 Kd = it.intersected()->material()->get_albedo(it);
+			float cos = shadowRay.dot(pN);
+			L += Kd * Id * cos;
+
+			// TERMINO ESPECULAR = Ks x Is x (R . V)^n
+			Vector3 Is = lt->get_incoming_light(pI);
+			Vector3 Ks = it.intersected()->material()->get_albedo(it);
+			Vector3 V = it.get_ray().get_direction();
+			V = V.normalize();
+			Vector3 R = shadowRay.reflect(pN).normalize();
+			cos = R.dot(V);
+			if (cos < 0.0) cos = 0.0;
+
+			L += Ks * Is * pow(cos, 80);
+
+		}
+	}
+
+	// LUZ INDIRECTA //
+
+	// Photon mapping algorithm for Global Illumination
+	std::vector<const KDTree<Photon, 3>::Node*> global_photons;
+	Real max_distance = 100;
+	m_global_map.find(std::vector<Real>(it.get_position().data, it.get_position().data + 3), m_nb_photons, global_photons, max_distance);
+
+	//cout << global_photons.size();
+
+	std::vector<const KDTree<Photon, 3>::Node*> causics_photons;
+	max_distance = 100;
+	m_caustics_map.find(std::vector<Real>(it.get_position().data, it.get_position().data + 3), m_nb_photons, causics_photons, max_distance);
+
+	if (global_photons.size() > 1) {
+		cout << global_photons.size() << "\n";
+	}
+	if (causics_photons.size() > 1) {
+		cout << causics_photons.size() << "\n";
+	}
+
+	cout << m_nb_photons << "\n";
+	return L;
 
 	//**********************************************************************
 	// The following piece of code is included here for two reasons: first
